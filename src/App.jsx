54d8296035d358eb33f0cb0c0507e1c277a90e6c
Vlad <vlad086367@gmail.com>
2025-09-14 13:09:11 +0300

    Improved card style

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import UserCard from "./components/userCard/index";
 function App() {
     const user = {
       userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrQtqi8wQTytBhnCU5o8SazhfyYpn9DPBWhw&s',
-      lastName: 'Test',
-      firstName: 'Testovych',
-      userName: 'UserTest',
-      gender: 'male',
+      lastName: 'Taylor',
+      firstName: 'Swift',
+      userName: '@Taylor123',
+      gender: 'Male',
       followers: 21,
+      likes: 5,
     }
   return <UserCard user={user}/>;
 }
--- src/components/userCard/index.jsx
+++ src/components/userCard/index.jsx
@@ -4,23 +4,26 @@ import React, { Component } from "react";
 class UserCard extends Component {
   render() {
     const { user } = this.props;
-    const { userImg, firstName, lastName, userName, gender, followers } = user;
+    const { userImg, firstName, lastName, userName, gender, followers, likes } = user;
     return (
-      <>
-        <main className={styles.main}>
-          <article className={styles.aticle}>
+      <main className={styles.main}>
+        <article className={styles.article}>
+          <p className={styles.userName}>{userName}</p>
+          <div className={styles.bio}>
             <img className={styles.userImg} src={userImg} alt={firstName} />
-            <div className={styles.userInfo}>
-              <p className={styles.firstName}>{firstName}</p>
-              <p className={styles.lastName}>{lastName}</p>
-            </div>
-            <p className={styles.userName}>{userName}</p>
-            <p className={styles.gender}>{gender}</p>
-            <p className={styles.followers}>{followers}</p>
             <button className={styles.btn}>+</button>
-          </article>
-        </main>
-      </>
+          </div>
+          <div className={styles.fullName}>
+            <p className={styles.lastName}>{lastName} </p>
+            <p className={styles.firstName}>{firstName}</p>
+          </div>
+          <p className={styles.gender}>Gender: {gender}</p>
+          <div className={styles.divFooter}>
+            <p className={styles.followers}>Followers: {followers}</p>
+            <p className={styles.userLike}>like: {likes}</p>
+          </div>
+        </article>
+      </main>
     );
   }
 }
