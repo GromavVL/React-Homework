c2cc900681a60751355c5718fd53efe4a4236c24
Vlad <166547154+GromavVL@users.noreply.github.com>
2025-12-31 13:20:23 +0200

    Add Sign up component

--- src/components/SignUpFormik/index.jsx
+++ src/components/SignUpFormik/index.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { SIGNAP_VALIDATION_SCHEMA } from '../../utils/validationSchema';
+import styles from './SignUpFormik.module.scss';
 
 function SignUpFormik () {
-  return <div>SignUpFormik</div>;
+  const loginInitialValues = {
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    acceptTerms: false,
+  };
+
+  const handleSubmit = (values, formikBag) => {
+    console.log('values :>> ', values);
+    formikBag.resetForm();
+  };
+
+  return (
+    <div className={styles.formWrapper}>
+      {/* <h1>Create Your Account</h1> */}
+      <Formik
+        initialValues={loginInitialValues}
+        onSubmit={handleSubmit}
+        validationSchema={SIGNAP_VALIDATION_SCHEMA}
+      >
+        {formikProps => {
+          return (
+            <Form className={styles.formBase}>
+              <h2>Sign up</h2>
+              <label>
+                <span>Full Name</span>
+                <Field type='text' name='fullName' autoFocus />
+                <ErrorMessage name='fullName' component={'span'} />
+              </label>
+              <label>
+                <span>Email</span>
+                <Field type='email' name='email' />
+                <ErrorMessage name='email' component={'span'} />
+              </label>
+              <label>
+                <span>Password</span>
+                <Field type='password' name='password' />
+                <ErrorMessage name='password' component={'span'} />
+              </label>
+              <label>
+                <span>Confirm Password</span>
+                <Field type='password' name='confirmPassword' />
+                <ErrorMessage name='confirmPassword' component={'span'} />
+              </label>
+              <label>
+                <span>I agree to the terms and conditions</span>
+                <Field type='checkbox' name='acceptTerms' />
+                <ErrorMessage name='acceptTerms' component={'span'} />
+              </label>
+              <button type='submit'>Sign up</button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
 }
 
 export default SignUpFormik;
---
+++ src/utils/validationSchema.js
@@ -0,0 +1,36 @@
+import * as yup from 'yup';
+
+export const SIGNAP_VALIDATION_SCHEMA = yup.object({
+    fullName: yup
+    .string()
+    .trim()
+    .min(2)
+    .max(64)
+    .matches(/^[A-Z]/, 'Name must starts wich capital letter ')
+    .required(),
+
+    email: yup
+    .string()
+    .email('Invalid email address')
+    .required('Email is required'),
+
+    password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters')
+    .max(32, 'Password must be at most 32 characters')
+    .matches(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])/,
+      'Password must include uppercase, lowercase, number and special character'
+    ),
+
+    confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required(),
+
+    acceptTerms: yup
+    .boolean()
+    .oneOf([true], 'You must accept the terms and conditions')
+    .required('You must accept the terms and conditions')
+})
